dc4696ce08061299e6790a2b56eafd19d5145db2
aalaa <alaamglal@gmail.com>
2024-08-22 23:46:35 +0300

    done

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+  {
+    "title": "Blog Post 1"
+  }
+  {
+    "title": "Blog Post 2"
+  }
+]
+
+
 # Create your views here.
 def home(request):
-  return HttpResponse("<h1>Blog Home</h1>")
+  context = {
+    "posts": posts
+  }
+  return render(request, "blog/home.html", context)
+
+def about(request):
+  return HttpResponse("<h1>Blog About</h1>")
