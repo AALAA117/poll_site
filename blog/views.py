e51b97dc8b472c58f1bd4501179dfe80823256b4
aalaa <alaamglal@gmail.com>
2024-08-23 01:34:20 +0300

    add about page and editing home

--- blog/views.py
+++ blog/views.py
@@ -2,14 +2,19 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 posts = [
-  {
-    "title": "Blog Post 1"
-  }
-  {
-    "title": "Blog Post 2"
-  }
-]
-
+    {
+        'author': 'CoreyMS',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2018'
+    }
+    ]
 
 # Create your views here.
 def home(request):
@@ -19,4 +24,4 @@ def home(request):
   return render(request, "blog/home.html", context)
 
 def about(request):
-  return HttpResponse("<h1>Blog About</h1>")
+  return render(request, "blog/about.html")
