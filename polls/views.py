7db7971bf697379522c7f5a938bf4eeab8b80c4c
aalaa <alaamglal@gmail.com>
2024-08-27 23:08:05 +0300

    done

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,7 @@
 from django.core.exceptions import PermissionDenied
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
 from django.template import loader
-
 from .models import Question
 
 
@@ -17,7 +16,11 @@ def index(request):
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, "polls/detail.html", {"question": question})
 
 
 def results(request, question_id):
